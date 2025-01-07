c924effe813f5d4729401355d18eda1ebf676cf8
otsomikkila <otso.mikkila@hotmail.fi>
2025-01-07 18:57:31 +0200

    Blog List Frontend, step 2

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,14 @@ const App = () => {
     )  
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedNoteappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+    }
+  }, [])
+
   const handleLogin = async (event) => {
     event.preventDefault()
     
@@ -23,7 +31,9 @@ const App = () => {
       const user = await loginService.login({
         username, password,
       })
-      console.log(user)
+      window.localStorage.setItem(
+        'loggedNoteappUser', JSON.stringify(user)
+      )
       setUser(user)
       setUsername('')
       setPassword('')
@@ -33,6 +43,11 @@ const App = () => {
         //setErrorMessage(null)
       }, 5000)
     }
+  }
+
+  const handleLogout = () => {
+    window.localStorage.removeItem('loggedNoteappUser')
+    setUser(null)
   }
 
   if (user === null) {
@@ -53,7 +68,10 @@ const App = () => {
   return (
     <div>
       <h2>blogs</h2>
-      <p>{user.name} logged in</p>
+      <div>
+        <p>{user.name} logged in </p>
+        <button onClick={() => handleLogout()}>logout</button>
+      </div>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
